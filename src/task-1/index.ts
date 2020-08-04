bff38c41bf37189c884138152253429b99cbf918
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2020-08-05 02:55:24 +0300

    Complete the task-1

---
+++ src/task-1/index.ts
@@ -0,0 +1,7 @@
+module.exports = () => {
+    process.stdin.on("data", (data) => {
+        const result = data.reverse().toString().trim() + "\n\n";
+    
+        process.stdout.write(result);
+    });
+}
