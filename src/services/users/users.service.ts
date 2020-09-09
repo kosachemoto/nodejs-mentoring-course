a8dcbee93e3137ca5577770ea5b000fb36a5a280
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2020-09-09 03:14:11 +0300

    Add UserService

---
+++ src/services/users/users.service.ts
@@ -0,0 +1,58 @@
+import { v4 } from 'uuid';
+import { UsersServiceTypes } from './users.types';
+
+export class UsersService implements UsersServiceTypes.Users {
+    users: UsersServiceTypes.User[] = [];
+
+    createUser(data: UsersServiceTypes.SaveData) {
+        const newUser: UsersServiceTypes.User = {
+            ...data,
+            id: v4(),
+            isDeleted: false,
+        };
+
+        this.users.push(newUser);
+    }
+
+    getUser(id: string) {
+        const user = this.users.find((user) => user.id === id && !user.isDeleted);
+
+        if (user === undefined) {
+            throw(new Error(`User with { id: \"${id}\" } doesn't exist.`));
+        }
+
+        return user;
+    }
+
+    getUsers() {
+        return this.users;
+    }
+
+    updateUser(data: UsersServiceTypes.UpdateData) {
+        const {
+            id,
+            ...restData
+        } = data;
+
+        const userIndex = this.users.findIndex((user) => user.id === id && !user.isDeleted);
+
+        if (userIndex === undefined) {
+            throw(new Error(`User with { id: \"${id}\" } doesn't exist.`));
+        }
+
+        this.users[userIndex] = {
+            ...this.users[userIndex],
+            ...restData,
+        }
+    };
+
+    deleteUser(id: string) {
+        const userIndex = this.users.findIndex((user) => user.id === id && !user.isDeleted);
+
+        if (userIndex === undefined) {
+            throw(new Error(`User with { id: \"${id}\" } doesn't exist.`));
+        }
+
+        this.users[userIndex].isDeleted = true;
+    }
+}
---
+++ src/services/users/users.service.types.ts
@@ -0,0 +1,22 @@
+export namespace UsersServiceTypes {
+    export type User = {
+        id: string;
+        login: string;
+        password: string;
+        age: string;
+        isDeleted: boolean;
+    }
+
+    export type SaveData = Omit<User, 'id' | 'isDeleted'>;
+
+    export type UpdateData = Pick<User, 'id'> & Partial<Omit<User, 'id' | 'isDeleted'>>;
+
+    export interface Users {
+        users: User[];
+        createUser: (data: SaveData) => void;
+        getUser: (id: string) => User;
+        getUsers: () => User[];
+        updateUser: (data: UpdateData) => void;
+        deleteUser: (id: string) => void;
+    };
+}
