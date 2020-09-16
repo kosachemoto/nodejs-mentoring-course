4c87310ebe845e4d464fc31c61e1e79b053f5b7d
Nikita Kosachev <Nikita_Kosachev@epam.com>
2020-09-16 16:17:58 +0300

    Fix User validation rules

--- src/validation-schemas/users/users.validation-schema.ts
+++ src/validation-schemas/users/users.validation-schema.ts
@@ -2,18 +2,21 @@ import Joi from 'joi';
 import { UsersValidationSchemaTypes } from './users.validation-schema.types';
 
 const id = Joi.string().required();
-const login = Joi.string().min(3).required();
-const password = Joi.string().alphanum().min(3).pattern(/[a-zA-Z]/, { name: 'letters' }).pattern(/[0-9]/, { name: 'numbers' });
-const age = Joi.number().min(4).max(400).required();
+const optionalLogin = Joi.string().min(3);
+const requiredLogin = optionalLogin.required();
+const optionalPassword = Joi.string().alphanum().min(3).pattern(/[a-zA-Z]/, { name: 'letters' }).pattern(/[0-9]/, { name: 'numbers' });
+const requiredPassword = optionalPassword.required();
+const optionalAge = Joi.number().min(4).max(400);
+const requiredAge = optionalAge.required();
 
 const loginSubstring = Joi.string();
 const limit = Joi.string();
 
 export const usersValidationSchema: UsersValidationSchemaTypes.Schema = {
     createUser: Joi.object().keys({
-        login,
-        password,
-        age,
+        login: requiredLogin,
+        password: requiredPassword,
+        age: requiredAge,
     }),
     getUser: Joi.object({
         id,
@@ -24,9 +27,9 @@ export const usersValidationSchema: UsersValidationSchemaTypes.Schema = {
     }),
     updateUser: Joi.object({
         id,
-        login,
-        password,
-        age,
+        login: optionalLogin,
+        password: optionalPassword,
+        age: optionalAge,
     }),
     deleteUser: Joi.object({
         id,
