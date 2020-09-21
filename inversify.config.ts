bd207fa49749638f55c94a00a8de6a92b89f0f4f
Nikita Kosachev <Nikita_Kosachev@epam.com>
2020-09-21 03:25:49 +0300

    FIll IoC

--- inversify.config.ts
+++ inversify.config.ts
@@ -2,7 +2,16 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 import { TYPES } from './inversify.types';
 
-import { options, OverloadedSequelize } from './orm';
+import { DataTypes } from 'sequelize';
+import { options, OverloadedSequelize } from './src/orm';
+import { 
+    UserDALModel,
+    UserDomainModel,
+    UserORMModel,
+} from './src/models/user';
+import { UserDataMapper } from '@root/src/data-mappers/user';
+import { UsersDAL } from './src/dal/users';
+import { UserRepository } from './src/repositories/users';
 import { UsersService } from './src/services/users';
 import { UsersController } from './src/controllers/users';
 import { usersValidationSchema } from './src/validation-schemas/users';
@@ -13,7 +22,18 @@ export const container = new Container({
 });
 
 container.bind(TYPES.ORM.DB_OPTIONS).toConstantValue(options);
+container.bind(TYPES.ORM.DATA_TYPES).toConstantValue(DataTypes);
 container.bind(TYPES.ORM.SEQUELIZE).to(OverloadedSequelize).inSingletonScope();
+
+container.bind(TYPES.MODELS.DAL.USER).toConstantValue(UserDALModel);
+container.bind(TYPES.MODELS.DOMAIN.USER).toConstantValue(UserDomainModel);
+container.bind(TYPES.MODELS.ORM.USER).to(UserORMModel).inSingletonScope();
+
+container.bind(TYPES.DATA_MAPPER.USER).to(UserDataMapper).inSingletonScope();
+
+container.bind(TYPES.DAL.USERS).to(UsersDAL).inSingletonScope();
+
+container.bind(TYPES.REPOSITORIES.USERS).to(UserRepository).inSingletonScope();
 
 container.bind(TYPES.USERS.SERVICE).to(UsersService).inSingletonScope();
 container.bind(TYPES.USERS.CONTROLLER).to(UsersController).inSingletonScope();
--- inversify.types.ts
+++ inversify.types.ts
@@ -1,12 +1,34 @@
+// TODO: Сделать нормальную структуру
 export const TYPES = {
     ORM: {
         DB_OPTIONS: Symbol('DB_OPTIONS'),
         SEQUELIZE: Symbol('SEQUELIZE'),
+        DATA_TYPES: Symbol('DATA_TYPES'),
+    },
+    DAL: {
+        USERS: Symbol('USERS_DAL'),
+    },
+    MODELS: {
+        DAL: {
+            USER: Symbol('USER_DAL_MODEL'),
+        },
+        DOMAIN: {
+            USER: Symbol('USER_DOMAIN_MODEL'),
+        },
+        ORM: {
+            USER: Symbol('USER_ORM_MODEL'),
+        },
+    },
+    DATA_MAPPER: {
+        USER: Symbol('USER_DATA_MAPPER'),
+    },
+    REPOSITORIES: {
+        USERS: Symbol('USERS_REPOSITORY'),
     },
     USERS: {
-        SERVICE: Symbol('SERVICE'),
-        VALIDATION: Symbol('VALIDATION'),
-        SCHEMA: Symbol('SCHEMA'),
-        CONTROLLER: Symbol('CONTROLLER'),
+        SERVICE: Symbol('USERS_SERVICE'),
+        VALIDATION: Symbol('USERS_VALIDATION'),
+        SCHEMA: Symbol('USERS_SCHEMA'),
+        CONTROLLER: Symbol('USERS_CONTROLLER'),
     },
 };
