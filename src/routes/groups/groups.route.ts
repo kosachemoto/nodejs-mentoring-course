e71cbbaae92801154eee8ef2e2e0b148d45a97eb
Nikita Kosachev <kosachemoto@gmail.com>
2020-10-19 02:17:09 +0300

    All verification rules for all controllers

--- src/routes/groups/groups.route.ts
+++ src/routes/groups/groups.route.ts
@@ -2,25 +2,28 @@ import express from 'express';
 import { TYPE } from '@ioc/inversify.types';
 import { container } from '@ioc/inversify.config';
 import { NGroupsController } from '@controllers/groups';
+import { NAuthenticationRules } from '@validation/authentication';
 
 import IGroupsController = NGroupsController.IGroupsController;
+import IAuthenticationRules = NAuthenticationRules.IAuthenticationRules;
 
 const groupsRoute = express.Router();
 
 const groupsController = container.get<IGroupsController>(TYPE.CONTROLLER.GROUP);
+const authenticationValidation = container.get<IAuthenticationRules>(TYPE.VALIDATION.RULES.AUTHENTICATION);
 
 groupsRoute.route('/')
-    .post(groupsController.createGroup)
-    .get(groupsController.getGroups);
+    .post(authenticationValidation.verify, groupsController.createGroup)
+    .get(authenticationValidation.verify, groupsController.getGroups);
 
 groupsRoute.route('/:id')
-    .get(groupsController.getGroup)
-    .put(groupsController.updateGroup)
-    .delete(groupsController.deleteGroup);
+    .get(authenticationValidation.verify, groupsController.getGroup)
+    .put(authenticationValidation.verify, groupsController.updateGroup)
+    .delete(authenticationValidation.verify, groupsController.deleteGroup);
     
 groupsRoute.route('/:id/users/')
-    .post(groupsController.addGroupUsers)
-    .get(groupsController.getGroupUsers);
+    .post(authenticationValidation.verify, groupsController.addGroupUsers)
+    .get(authenticationValidation.verify, groupsController.getGroupUsers);
 
 
 export { groupsRoute };
--- src/validation/authentication/rules/authentication.rules.ts
+++ src/validation/authentication/rules/authentication.rules.ts
@@ -1,18 +1,23 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'inversify';
+import jwt from 'jsonwebtoken';
 import { TYPE } from '@ioc/inversify.types';
 import { NAuthenticationSchema } from '../schema';
 import { IAuthenticationRules } from './authentication.rules.types';
+import { NAuthentication } from 'src/authentication';
 
 
 @injectable()
 export class AuthenticationRules implements IAuthenticationRules {
     schema: NAuthenticationSchema.IAuthenticationSchema;
+    options: NAuthentication.TOptions;
 
     constructor(
-        @inject(TYPE.VALIDATION.SCHEMA.APPLICATION) schema: NAuthenticationSchema.IAuthenticationSchema,
+        @inject(TYPE.VALIDATION.SCHEMA.AUTHENTICATION) schema: NAuthenticationSchema.IAuthenticationSchema,
+        @inject(TYPE.AUTHENTICATION.OPTIONS) options: NAuthentication.TOptions,
     ) {
         this.schema = schema;
+        this.options = options;
     }
 
     login: IAuthenticationRules['login'] = async (req, res, next) => {
@@ -23,5 +28,27 @@ export class AuthenticationRules implements IAuthenticationRules {
         } else {
             res.status(400).send(error);
         }
+    }
+
+    verify: IAuthenticationRules['verify'] = async (req, res, next) => {
+        const accessToken = await Promise.resolve(req.cookies.jwt).then((accessToken) => {
+            if (!accessToken) {
+                throw new Error();
+            }
+
+            return accessToken;
+        }).catch((error) => {
+            res.status(401).send();
+
+            throw error;
+        });
+        
+        await Promise.resolve(accessToken).then((accessToken) => {
+            jwt.verify(accessToken, this.options.ACCESS_TOKEN_SECRET)
+        }).then(() => {
+            next();
+        }).catch((error) => {
+            return res.status(403).send(error);
+        });
     }
 }
