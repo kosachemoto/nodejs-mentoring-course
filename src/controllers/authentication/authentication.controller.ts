fee42c6a80807b6d557928ffa3c9894c6e5aaa66
Nikita Kosachev <kosachemoto@gmail.com>
2020-10-19 02:36:12 +0300

    Add logging

--- src/controllers/authentication/authentication.controller.ts
+++ src/controllers/authentication/authentication.controller.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { injectable, inject } from 'inversify';
+import winston from 'winston';
 import { TYPE } from '@ioc/inversify.types';
 import { NAuthenticationService } from '@services/authentication';
 import { IAuthenticationController } from './authentication.controller.types';
 import { NAuthentication } from 'src/authentication';
+import { errorLoggerFormat } from "@utils/.";
 
 import IAuthenticationService = NAuthenticationService.IAuthenticationService;
 
@@ -11,13 +13,16 @@ import IAuthenticationService = NAuthenticationService.IAuthenticationService;
 export class AuthenticationController implements IAuthenticationController {
     applicationService: IAuthenticationService;
     options: NAuthentication.TOptions;
+    logger: winston.Logger
 
     constructor(
         @inject(TYPE.SERVICE.AUTHENTICATION) applicationService: IAuthenticationService,
         @inject(TYPE.AUTHENTICATION.OPTIONS) options: NAuthentication.TOptions,
+        @inject(TYPE.WINSTON.LOGGER) logger: winston.Logger,
     ) {
         this.applicationService = applicationService;
-        this.options = options;
+        this.options = options;0
+        this.logger = logger;
     }
 
     login: IAuthenticationController['login'] = async (req, res) => {
@@ -30,6 +35,10 @@ export class AuthenticationController implements IAuthenticationController {
                 { httpOnly: true, expires: new Date(Date.now() + this.options.ACCESS_TOKEN_LIFE) });
         }).then(() => {
             res.send();
+        }).catch((error) => {
+            this.logger.error(errorLoggerFormat(error));
+
+            throw error;
         }).catch((error) => {
             res.status(401).send(error);
         });
